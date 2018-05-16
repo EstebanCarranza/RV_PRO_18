782f03c5f9a15c00367541a213ba7c8957fdb6f5
Trecan <39281428+Trecan@users.noreply.github.com>
2018-05-16 13:41:50 -0500

    BP y audio de gaviota
    BP con el skeletal mesh de la gaviota junto con el cue con graznidos reproducido aleatoriamente cada fracción de tiempo aleatoria para dar ambientación.

---
+++ Plugin/UE4Duino/Intermediate/Build/Win64/UE4Editor/Inc/UE4Duino/UE4Duino.generated.dep.h
@@ -0,0 +1,10 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#ifndef UE4DUINO_Serial_generated_h
+	#include "Private/Serial.h"
+#endif
